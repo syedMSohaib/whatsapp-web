59a843d07af27300458ce9cf9bb0af33ae0ee6ba
Syed M Sohaib <nytrotech.dev@gmail.com>
2021-06-03 11:50:23 +0500

    fix baseurl

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -38,6 +38,11 @@ if (token) {
 	console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
+if(process.env.NODE_ENV == 'production')
+    window.axios.defaults.baseURL =  '/whatsapp-web';
+else
+    window.axios.defaults.baseURL =  '';
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
